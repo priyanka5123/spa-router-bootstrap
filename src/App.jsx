ad611212c122c38063cc0e1a9ecaf08b424f4900
priyanka5123 <priyanka.k8100@gmail.com>
2026-01-30 20:13:02 -0700

    added forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import NotFound from './components/NotFound';
 import Users from './components/Users';
 import UserTodos from './components/UserTodos';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<Users />} />
 				<Route path="/user-todos/:userId" element={<UserTodos />} />
+				<Route path="/user-form" element={<UserForm />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
